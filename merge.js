09579713e94097eeadb54abd603904cfc69407dd
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-12 10:58:32 +0000

    More updated code

---
+++ merge.js
@@ -0,0 +1,9 @@
+function merge(array1, array2){
+	for (let i = 0; i < array1.length; i++){
+		for (let j = 0; j < array2.lengthl j++){
+			if (array2[j] < array1[i]){
+				
+			}
+		}
+	}
+}
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-let sum = 3+2
+const bins = {
+	 waste: 4,
+	 recycling: 3,
+	 compost: 5
+	 }
 
-const doubleSum = function(a,b) {
-	let sum = a + b;
-	return sum * 2;
+const keys = Object.keys(bins)
+console.log(keys);
+for (let i = 0; i < keys.length; i++){
+	console.log(keys[i]);
 }
-
-doubleSum(5,4);
-
-console.log(sum);

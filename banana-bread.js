7278781b15f99ceee416bf33515374faea878e0d
Brandon Macleod <BrandonMacleod21@gmail.com>
2020-05-12 02:40:39 +0000

    banana bread lists

---
+++ banana-bread.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+
+while (i < ingredients.length){
+	console.log(ingredients[i]);
+	i++
+}
+
+console.log();
+
+for (let j = 0; j < ingredients.length; j++){
+	console.log(ingredients[j]);
+}
+
+console.log();
+
+for (let k = ingredients.length-1; k >=0; k--){
+	console.log(ingredients[k]);
+}
+
